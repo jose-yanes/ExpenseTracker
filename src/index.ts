22654c6738ff926bbd996d6d030575b79861af13
Jose Yanes <113186145+jose-yanes@users.noreply.github.com>
2025-06-26 10:34:59 -0300

    Adding Logger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { timeout } from "hono/timeout";
+import { logger } from "hono/logger";
 import { Routes } from "./routes";
 const app = new Hono();
 
+app.use("*", logger());
 app.use("/notion/sync_db", timeout(20000));
 app.route("/notion", Routes.notionRoutes);
 export default app;
